b8ec7ff1508bb9152f27091b97bb5bebab4d919f
Julien Capellari <julien@capellari.net>
2019-04-28 19:29:06 +0200

    update: convert JNIClass to kotlin

---
+++ src/main/java/net/capellari/julien/threed/jni/JNIClass.kt
@@ -0,0 +1,25 @@
+package net.capellari.julien.threed.jni
+
+abstract class JNIClass {
+    // Companion
+    companion object {
+        // Initialisation classe
+        init {
+            System.loadLibrary("android3d")
+        }
+    }
+
+    // Attributs
+    @JvmField
+    val nativeHandle: Long
+
+    // Constructeur
+    protected constructor(nativeHandle: Long) {
+        this.nativeHandle = nativeHandle
+        assert(acquire())
+    }
+
+    // Méthodes
+    private external fun acquire(): Boolean
+    external fun dispose()
+}
