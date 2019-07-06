770b844c0bacda0e4b221475bd4a076a5796825a
Julien Capellari <julien@capellari.net>
2019-07-06 14:21:04 +0200

    update: use weak references

--- src/main/java/net/capellari/julien/threed/jni/JNIRegister.kt
+++ src/main/java/net/capellari/julien/threed/jni/JNIRegister.kt
@@ -1,20 +1,22 @@
 package net.capellari.julien.threed.jni
 
+import java.lang.ref.WeakReference
+
 class JNIRegister<T: JNIClass>(private val factory: (Long) -> T) {
     // Attributs
-    private val register = mutableMapOf<Long,T>()
+    private val register = mutableMapOf<Long,WeakReference<T>>()
 
     // Methods
     fun add(obj: T) {
-        register[obj.nativeHandle] = obj
+        register[obj.nativeHandle] = WeakReference(obj)
     }
 
     fun get(handle: Long): T {
-        var obj = register[handle]
+        var obj = register[handle]?.get()
 
         if (obj == null) {
             obj = factory(handle)
-            register[handle] = obj
+            register[handle] = WeakReference(obj)
         }
 
         return obj
