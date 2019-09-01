c54b99b4c7c437363de9fe9ec25c8fb158a3490f
Julien Capellari <julien@capellari.net>
2019-09-01 10:43:33 +0200

    add: all vector array's JNI

--- src/main/jni/math/array.cpp
+++ src/main/jni/math/array.cpp
@@ -7,3 +7,9 @@ using namespace math;
 
 // JNI
 VECTOR_ARRAY_JNI(VectorArray2i, Vec2i, jint)
+VECTOR_ARRAY_JNI(VectorArray3i, Vec3i, jint)
+VECTOR_ARRAY_JNI(VectorArray4i, Vec4i, jint)
+
+VECTOR_ARRAY_JNI(VectorArray2f, Vec2f, jfloat)
+VECTOR_ARRAY_JNI(VectorArray3f, Vec3f, jfloat)
+VECTOR_ARRAY_JNI(VectorArray4f, Vec4f, jfloat)
